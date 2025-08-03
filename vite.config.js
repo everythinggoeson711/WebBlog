91aa6ec88d266ecec657c30a4869692df8dcdfd9
vinny <doanvinhnguyen0711@gmail.com>
2025-08-03 13:25:28 +0700

    Fix SCSS configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+    cssCodeSplit: true,
+    minify: 'terser',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/_variables.scss";`,
+      },
+    },
+    devSourcemap: true,
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'public, max-age=31536000',
+      'Content-Type': 'auto',
+    },
+  },
 })
